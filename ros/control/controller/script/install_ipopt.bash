27e153a5996919c17ed38885e1e89db0e037b165
Feng Yunji <1120192186@bit.edu.cn>
2021-04-27 21:26:18 +0800

    update scripts

--- ros/control/controller/script/install_ipopt.bash
+++ ros/control/controller/script/install_ipopt.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Pass the Ipopt source directory as the first argument
 
-sudo apt-get -y install gfortran
+#sudo apt-get -y install gfortran
 
-if ( ldconfig -p | grep libipopt ); then
-    echo "Ipopt is already installed......."
-    exit
-fi
+#if ( ldconfig -p | grep libipopt ); then
+#    echo "Ipopt is already installed......."
+#    exit
+#fi
 
 echo "Downloading Ipopt-3.12.7"
 
@@ -19,6 +19,31 @@ srcdir=$PWD
 echo "Building Ipopt from ${srcdir}"
 echo "Saving headers and libraries to ${prefix}"
 
+# BLAS
+cd $srcdir/ThirdParty/Blas
+./get.Blas
+mkdir -p build && cd build
+../configure --prefix=$prefix --disable-shared --with-pic
+make install
+
+# Lapack
+cd $srcdir/ThirdParty/Lapack
+./get.Lapack
+mkdir -p build && cd build
+../configure --prefix=$prefix --disable-shared --with-pic \
+    --with-blas="$prefix/lib/libcoinblas.a -lgfortran"
+make install
+
+# ASL
+cd $srcdir//ThirdParty/ASL
+./get.ASL
+
+# MUMPS
+cd $srcdir/ThirdParty/Mumps
+./get.Mumps
+
+# build everything
+cd $srcdir
 echo "Configuring and building IPOPT"
 ./configure --prefix /usr/local
 
@@ -33,4 +58,3 @@ fi
 sudo ldconfig
 echo "IPOPT installed successfully"
 source $HOME/.bashrc
-rm -r Ipopt-3.12.7
